7db4c6e6d133f8e938e6d1e8c2d6dfee5f138ae1
nissim.hadar <nissim.hadar@gmail.com>
2017-08-08 21:18:59 -0700

    Corrected miss-spelled #define.

--- libraries/gl/src/gl/ContextQt.cpp
+++ libraries/gl/src/gl/ContextQt.cpp
@@ -28,7 +28,7 @@ void Context::makeCurrent(QOpenGLContext* context, QSurface* surface) {
 }
 
 QOpenGLContext* Context::qglContext() {
-#if defined Q_OS_WIN && !defined Q_OS_WIN
+#if defined Q_OS_WIN && !defined Q_OS_WINRT
     if (!_wrappedContext) {
         _wrappedContext = new QOpenGLContext();
         _wrappedContext->setNativeHandle(QVariant::fromValue(QWGLNativeContext(_hglrc, _hwnd)));
