5393d138a07f0e5beb7cd98e6d4e5348bbadecf3
Nissim Hadar <nissim@highfidelity.io>
2017-08-15 10:49:40 -0700

    Added a delay of 2 seconds after renaming gl folders.

--- make UWP.bat
+++ make UWP.bat
@@ -6,6 +6,9 @@ REM Use UWP display plugin sources
 ren libraries\display-plugins\src src_WIN
 ren libraries\display-plugins\src_UWP src
 
+REM Sleep 2 seconds - the renaming seems to take some time
+ping -n 3 127.0.0.1 > NUL
+
 cd build-UWP
 cmake .. -G "Visual Studio 15 Win64" -DUWP=TRUE
 cd ..
