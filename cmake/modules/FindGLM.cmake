5e8902637ec4d92957d4239f7269dedc9bdb837d
Nissim Hadar <nissim@highfidelity.io>
2017-08-16 17:27:09 -0700

    Use vcpkg'ed version of glm in UWP.

--- cmake/modules/FindGLM.cmake
+++ cmake/modules/FindGLM.cmake
@@ -18,7 +18,11 @@ include("${MACRO_DIR}/HifiLibrarySearchHints.cmake")
 hifi_library_search_hints("glm")
 
 # locate header
-find_path(GLM_INCLUDE_DIRS "glm/glm.hpp" HINTS ${GLM_SEARCH_DIRS})
+if (UWP)
+  set (GLM_INCLUDE_DIRS "ENV{VCPKG_PATH}/include")
+else ()
+  find_path(GLM_INCLUDE_DIRS "glm/glm.hpp" HINTS ${GLM_SEARCH_DIRS})
+endif ()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE_DIRS)
