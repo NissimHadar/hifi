0ce5d777b1d273586789a5064ba67d57c639b893
NissimHadar <nissim.hadar@gmail.com>
2018-02-07 23:03:54 -0800

    Improved star image by modulating brightness with cosine of off-angle.

--- tools/cubemap-creator/src/Creator.cpp
+++ tools/cubemap-creator/src/Creator.cpp
@@ -283,7 +283,8 @@ void Creator::drawStars(QList<Star*> stars) {
 
                     double angle_rad = acos(glm::dot(pixelPos, star->position) / pixelPosLength);
                     if (angle_rad <= star->halfAngle_rads) {
-                        float modulationFactor = (star->halfAngle_rads - angle_rad) / star->halfAngle_rads;
+                        // The modulation factor is a small power of the angle cosine.  This is a number that is usually close to 1
+                        float modulationFactor = pow((star->halfAngle_rads - angle_rad) / star->halfAngle_rads, 0.2);
                         rawBufferR[offset] = star->color.r  * modulationFactor;
                         rawBufferG[offset] = star->color.g  * modulationFactor;
                         rawBufferB[offset] = star->color.b  * modulationFactor;
